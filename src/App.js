752b18ccf3493f4f90418b0715316b7bf15fe1cc
Sameer Ahmed <sameer003r@gmail.com>
2022-01-06 21:58:59 +0530

    Tailwind Init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import './styles/output.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="bg-gray-900 h-screen w-screen flex justify-center items-center">
+      <h3 className="text-white italic">Tailwind CSS</h3>
     </div>
   );
 }
