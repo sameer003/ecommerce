d666a30a1f4d45eb2e170d53e0e20a2bcbd8a0e0
Sameer Ahmed <sameer003r@gmail.com>
2022-01-07 21:45:04 +0530

    Redux Init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './styles/output.css';
+import "./styles/output.css";
+import {useSelector, useDispatch} from 'react-redux';
+import { increaseCounter, decreaseCounter} from './redux/reducers/Counter/counter.actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
   return (
-    <div className="bg-gray-900 h-screen w-screen flex justify-center items-center">
+    <div className="bg-gray-900 h-screen w-screen flex items-center justify-around">
       <h3 className="text-white italic">Tailwind CSS</h3>
+      <div  className="text-white italic">Count: {counter.count}</div>
+
+      <button onClick={() => dispatch(increaseCounter())} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-3 rounded">Increase Count</button>
+
+      <button onClick={() => dispatch(decreaseCounter())} className="bg-red-500  hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Decrease Count</button>
     </div>
   );
 }
